0879b6a5bcea55a68624372c3d36b9a95386df06
Edwin Agbenyega <eddybanks03@gmail.com>
2016-08-10 22:04:10 -0600

    testing regular materialize navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,21 +9,8 @@
 //= require_tree .
 
 
-var vue_function = function() {
-  var user = {
-    name: true
-  }
-
-  var vue_inst = new Vue({
-    el: '.jumbotron',
-    data: user
-  })
-}
-
 
 var startup = function() {
-  vue_function();
-
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
